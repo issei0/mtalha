accde4db0e6145e56a8bc06ffc3f57448433592e
issei001 <talhanawaz106@gmail.com>
2021-12-05 14:44:51 +0500

    variables

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#-ui#m&$+pxaf(q@5_gaxdn5gb%*)t8qs0!nz91yl*=o0*yjnv'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
