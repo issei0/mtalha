2625994a047424fa986a9076129973f0c6f30a23
issei001 <talhanawaz106@gmail.com>
2021-12-06 11:07:54 +0500

    django version 2.2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,5 +149,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
