96602387da781ef9ffc2d50427cfbbb5f4e27a5c
issei001 <talhanawaz106@gmail.com>
2021-12-06 10:41:11 +0500

    add db sqlite

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,6 +79,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
